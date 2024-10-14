60bfaf7f87e6623de5f976428319beb7aabdc864
arkadiew <113170277+arkadiew@users.noreply.github.com>
2024-10-14 19:58:43 +0300

    ...

--- TSCN/flock.gd
+++ TSCN/flock.gd
@@ -1,26 +1,12 @@
 extends Node3D
 
-# Массив для хранения всех рыб
+# Массив рыб
 var fishes : Array = []
 
-# Количество рыб
-var fish_count : int = 20
 
-# Префаб рыбы (сцена Fish)
-@export var fish_scene : PackedScene
 
 func _ready():
-	if fish_scene == null:
-		print("Error: fish_scene is not assigned!")
-		return
+	# Находим все дочерние объекты типа "Node3D", заменив CSGBox3D
+	fishes = get_children()
 
-	# Создаём рыбу и добавляем их в стаю
-	for i in range(fish_count):
-		var fish_instance = fish_scene.instantiate()
-
-		# Устанавливаем начальное положение рыбы в случайную позицию
-		fish_instance.global_transform.origin = Vector3(randf_range(-10, 10), randf_range(-10, 10), randf_range(-10, 10))
-
-		# Добавляем рыбу на сцену и в массив
-		add_child(fish_instance)
-		fishes.append(fish_instance)
+	
